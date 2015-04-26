2d198923d9112bc3a6aa9f00fc998d1995354b41
grafa <hrdina.pavel@gmail.com>
2015-04-26 22:34:13 +0200

    Added: test.py to ignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pygun",
-    version="0.1.1",
+    version="0.2",
     description="Simple python module for communicating with Mailgun API.",
     license="New BSD License",
     author="Pavel Hrdina",
